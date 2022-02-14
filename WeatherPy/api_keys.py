d01df71d740f970b7d5106e6ed937b3ae01a233d
Shreestina Tamrakar <tshreestina@gmail.com>
2022-02-14 22:59:31 +0800

    Updated

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "407257b024b6a0c8506b910fb84dbfe8"
 
 # Google API Key
 g_key = "AIzaSyCHWB9bCD3aW7RxejXxbXsvfP2Y33-a130"
